97af28ea64c19c4c63409e5d716479ae85f9769c
Athar Laique <atharlaique15@gmail.com>
2021-03-11 12:02:13 +0500

    SETUP: .env setup

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-
-app.use( morgan('tiny'))
+require('dotenv').config()
+const port = process.env.PORT
+//Middleware
+app.use( morgan('tiny') )
+//Routes
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
- 
-app.listen(3000,()=>{
-    console.log(`Server is running on port 3000`)
+//Server
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
 })
